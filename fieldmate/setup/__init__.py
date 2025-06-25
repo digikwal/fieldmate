fbef319944bb34af174c760231d2e850c0c31861
Digikwal <79085106+digikwal@users.noreply.github.com>
2025-06-25 15:08:47 +0200

    chore: setup module shortcuts

--- fieldmate/setup/__init__.py
+++ fieldmate/setup/__init__.py
@@ -1,1 +1,7 @@
 # fieldmate/setup/__init__.py
+
+# shortcut
+from .custom_fields import setup_custom_fields
+from .export_fields import export_fieldmate_fields
+
+__all__ = ["setup_custom_fields", "export_fieldmate_fields"]
